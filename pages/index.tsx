b40d3534c800e53c76d7f9c1341d2d75cba75e66
Gerrod Schirtzinger <gerrod.l.schirtzinger@gmail.com>
2022-10-17 11:15:12 -0400

    copying files from starter

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,56 @@
-import type { NextPage } from 'next';
+import Container from '../components/container';
+import MoreStories from '../components/more-stories';
+import HeroPost from '../components/hero-post';
+import Intro from '../components/intro';
+import Layout from '../components/layout';
+import { getAllPosts } from '../lib/api';
 import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+import { CMS_NAME } from '../lib/constants';
+import Post from '../interfaces/post';
 
-const Home: NextPage = () => {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Best Seen On Foot</title>
-        <meta name='description' content='Best Seen On Foot Travel Blog' />
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>Best Seen On Foot</h1>
-
-        <p className={styles.description}>Coming soon...</p>
-      </main>
-
-      <footer className={styles.footer}>Copyright 2022</footer>
-    </div>
-  );
+type Props = {
+  allPosts: Post[];
 };
 
-export default Home;
+export default function Index({ allPosts }: Props) {
+  const heroPost = allPosts[0];
+  const morePosts = allPosts.slice(1);
+  return (
+    <>
+      <Layout>
+        <Head>
+          <title>Next.js Blog Example with {CMS_NAME}</title>
+        </Head>
+        <Container>
+          <Intro />
+          {heroPost && (
+            <HeroPost
+              title={heroPost.title}
+              coverImage={heroPost.coverImage}
+              date={heroPost.date}
+              author={heroPost.author}
+              slug={heroPost.slug}
+              excerpt={heroPost.excerpt}
+            />
+          )}
+          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+        </Container>
+      </Layout>
+    </>
+  );
+}
+
+export const getStaticProps = async () => {
+  const allPosts = getAllPosts([
+    'title',
+    'date',
+    'slug',
+    'author',
+    'coverImage',
+    'excerpt',
+  ]);
+
+  return {
+    props: { allPosts },
+  };
+};
