12c0bea818e61e68e09d2b59922de949e8148a66
Gerrod Schirtzinger <gerrod.l.schirtzinger@gmail.com>
2023-02-27 14:24:15 -0500

    adding cypress

---
+++ src/pages/about-us.cy.ts
@@ -0,0 +1,15 @@
+import AboutUs from "./about-us";
+
+describe("<AboutUs />", () => {
+  it("should render and display expected content", () => {
+    // Mount the React component for the About page
+    cy.mount(<AboutUs />);
+
+    // The new page should contain an h1 with "About Us"
+    cy.get("h1").contains("About Us");
+
+    // Validate that a link with the expected URL is present
+    // *Following* the link is better suited to an E2E test
+    cy.get('a[href="/"]').should("be.visible");
+  });
+});
