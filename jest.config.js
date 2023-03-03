fe556f27714f12bdc9974d63b644c87e15936685
Gerrod Schirtzinger <gerrod.l.schirtzinger@gmail.com>
2023-03-03 16:09:48 -0500

    tests are passing

--- jest.config.js
+++ jest.config.js
@@ -12,6 +12,7 @@ const customJestConfig = {
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 
   testEnvironment: "jest-environment-jsdom",
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)"],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
