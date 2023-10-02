7f19997dd6ba9fc52b06219dbed866a8df40f8bd
Gerrod Schirtzinger <gerrod.l.schirtzinger@gmail.com>
2023-10-02 16:51:22 -0400

    fix typing issue

--- src/components/post-body.tsx
+++ src/components/post-body.tsx
@@ -1,43 +1,6 @@
-import ReactMarkdown, { Components } from "react-markdown";
 import Image from "next/image";
 import Link from "next/link";
-
-const components: Components = {
-  // eslint-disable-next-line no-unused-vars
-  a: ({ node, children, href, ...props }) => {
-    if (/(youtube-nocookie|youtube)\.com/.test(href ?? "")) {
-      return (
-        <iframe
-          width="560"
-          height="315"
-          src={href}
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
-      );
-    }
-    return (
-      <Link href={href ?? ""} {...props}>
-        {children}
-      </Link>
-    );
-  },
-  img: ({ src, alt, title }) => {
-    return (
-      <Image
-        src={src ?? ""}
-        alt={alt ?? ""}
-        title={title}
-        width="1036"
-        height="691"
-        placeholder="blur"
-        blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mM0uTyzHgAFBAIh7MnC9QAAAABJRU5ErkJggg=="
-      />
-    );
-  },
-};
+import ReactMarkdown from "react-markdown";
 
 type Props = {
   content: string;
@@ -49,7 +12,46 @@ const PostBody = ({ content }: Props) => {
       className="prose mb-5 max-w-none md:prose-lg lg:prose-xl"
       itemProp="articleBody"
     >
-      <ReactMarkdown components={components}>{content}</ReactMarkdown>
+      <ReactMarkdown
+        components={{
+          // eslint-disable-next-line no-unused-vars
+          a: ({ node, children, href, ref, ...props }) => {
+            if (/(youtube-nocookie|youtube)\.com/.test(href ?? "")) {
+              return (
+                <iframe
+                  width="560"
+                  height="315"
+                  src={href}
+                  title="YouTube video player"
+                  style={{ border: "none" }}
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                ></iframe>
+              );
+            }
+            return (
+              <Link href={href ?? ""} {...props}>
+                {children}
+              </Link>
+            );
+          },
+          img: ({ src, alt, title }) => {
+            return (
+              <Image
+                src={src ?? ""}
+                alt={alt ?? ""}
+                title={title}
+                width="1036"
+                height="691"
+                placeholder="blur"
+                blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mM0uTyzHgAFBAIh7MnC9QAAAABJRU5ErkJggg=="
+              />
+            );
+          },
+        }}
+      >
+        {content}
+      </ReactMarkdown>
     </div>
   );
 };
