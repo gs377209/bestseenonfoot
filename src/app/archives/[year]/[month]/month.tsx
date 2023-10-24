43b6c3be72efc9430fba0e0ad22a96fefc473e36
Gerrod Schirtzinger <gerrod.l.schirtzinger@gmail.com>
2023-10-24 11:59:05 -0400

    archives

---
+++ src/app/archives/[year]/[month]/month.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import Container from "@/components/container";
+import DateFormatter from "@/components/date-formatter";
+import MoreStories from "@/components/more-stories";
+import SideBar from "@/components/side-bar";
+import { Post } from "@/interfaces/post";
+
+interface Props {
+  allPosts: Post[];
+  allPostsByDate: Post[];
+}
+
+export default function MonthArchives({ allPosts, allPostsByDate }: Props) {
+  const firstPost = allPostsByDate[0];
+
+  return (
+    <Container>
+      <section className="mx-auto mb-32 lg:col-span-2">
+        <h1 className="mb-5 text-5xl font-bold leading-tight tracking-tighter md:pr-8 md:text-7xl">
+          Posts from{" "}
+          <DateFormatter
+            dateString={firstPost.date}
+            customFormat="LLLL, yyyy"
+          />
+        </h1>
+        <MoreStories posts={allPostsByDate} hideHeader />
+      </section>
+      <SideBar allPosts={allPosts} />
+    </Container>
+  );
+}
---
+++ src/app/archives/archives.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import Container from "@/components/container";
+import MoreStories from "@/components/more-stories";
+import SideBar from "@/components/side-bar";
+import { Post } from "@/interfaces/post";
+
+interface Props {
+  allPosts: Post[];
+}
+
+export default function Archives({ allPosts }: Props) {
+  return (
+    <Container>
+      <section className="mx-auto mb-32 lg:col-span-2">
+        <h1 className="text-5xl font-bold leading-tight tracking-tighter md:pr-8 md:text-7xl">
+          Archives
+        </h1>
+        <MoreStories posts={allPosts} hideHeader />
+      </section>
+      <SideBar allPosts={allPosts} />
+    </Container>
+  );
+}
