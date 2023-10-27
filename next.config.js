dc77e9e4b5bf1162fabc1a8ce3d9b6dbbf249bb5
Gerrod Schirtzinger <gerrod.l.schirtzinger@gmail.com>
2023-10-27 14:33:03 -0400

    fix contact form and pwa

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
-/** @type {import('next').NextConfig} */
 const withPWA = require("next-pwa")({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
 });
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    // needed for PWA currently
+    webpackBuildWorker: true,
+  },
   images: {
     formats: ["image/avif", "image/webp"],
     remotePatterns: [
--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -21,10 +21,34 @@ export default function manifest(): MetadataRoute.Manifest {
         src: "/icon.png",
         type: "image/png",
       },
+      {
+        purpose: "maskable",
+        sizes: "512x512",
+        src: "/icon.png",
+        type: "image/png",
+      },
     ],
     id: "/",
     name: "Best Seen On Foot",
     orientation: "portrait",
+    screenshots: [
+      {
+        // @ts-expect-error this is a real prop
+        form_factor: "wide",
+        label: "Homescreen of Bestseenonfoot",
+        sizes: "1440x797",
+        src: "/assets/screenshots/desktop-screenshot.png",
+        type: "image/png",
+      },
+      {
+        // @ts-expect-error this is a real prop
+        form_factor: "narrow",
+        label: "Homescreen of Bestseenonfoot",
+        sizes: "750x1334",
+        src: "/assets/screenshots/mobile-screenshot.png",
+        type: "image/png",
+      },
+    ],
     short_name: "Best Seen",
     start_url: "/",
     theme_color: "#34D399",
