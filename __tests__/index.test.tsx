67734dbc51446cf0e0d1983dc278718fc011de82
Gerrod Schirtzinger <gerrod.l.schirtzinger@gmail.com>
2024-01-23 14:23:58 -0500

    update jest testing

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,6 +1,5 @@
-import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
 import Intro from "@/components/intro";
+import { render, screen } from "@testing-library/react";
 
 describe("Intro", () => {
   it("renders a heading", () => {
