725a8f9f16a7f92ba89c5461fab6bc6cba63990b
Gerrod Schirtzinger <gerrod.l.schirtzinger@gmail.com>
2024-05-01 15:21:43 -0400

    add all the share buttons

--- src/components/ShareButtons.tsx
+++ src/components/ShareButtons.tsx
@@ -1,6 +1,27 @@
 "use client";
 
-import Link from "next/link";
+import {
+  EmailShare,
+  FacebookShare,
+  GabShare,
+  HatenaShare,
+  InstapaperShare,
+  LineShare,
+  LinkedinShare,
+  LiveJournalShare,
+  MailruShare,
+  PinterestShare,
+  PocketShare,
+  RedditShare,
+  TelegramShare,
+  TumblrShare,
+  TwitterShare,
+  VKShareShare,
+  ViberShare,
+  WeiboShare,
+  WhatsappShare,
+  WorkplaceShare,
+} from "react-share-lite";
 
 interface Props {
   shareURL: string;
@@ -8,39 +29,28 @@ interface Props {
 
 export default function ShareButtons({ shareURL }: Props) {
   return (
-    <div className="mb-5 flex">
-      <div
-        className="fb-share-button mr-2"
-        data-href={shareURL}
-        data-layout="button"
-        data-size="large"
-        data-lazy="true"
-      >
-        <Link
-          key={`share-buttons-fb-${shareURL}`}
-          target="_blank"
-          href={`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
-            shareURL,
-          )}&amp;src=sdkpreparse`}
-          className="fb-xfbml-parse-ignore"
-          rel="noreferrer noopener"
-        >
-          Share
-        </Link>
-      </div>
-      <Link
-        key={`share-buttons-twitter-${shareURL}`}
-        href="https://twitter.com/share?ref_src=twsrc%5Etfw"
-        className="twitter-share-button"
-        data-size="large"
-        data-text="Check out this article from Best Seen on Foot!"
-        data-via="bestseenonfoot"
-        data-hashtags="travel"
-        data-show-count="true"
-        data-lazy="true"
-      >
-        Tweet
-      </Link>
+    <div className="mb-5 grid grid-cols-4 gap-4 sm:grid-cols-6 md:grid-cols-8 2xl:grid-cols-12">
+      <FacebookShare url={shareURL} />
+      <TwitterShare url={shareURL} />
+      <LinkedinShare url={shareURL} />
+      <WhatsappShare url={shareURL} />
+
+      <EmailShare url={shareURL} />
+      <GabShare url={shareURL} />
+      <HatenaShare url={shareURL} />
+      <InstapaperShare url={shareURL} />
+      <LineShare url={shareURL} />
+      <LiveJournalShare url={shareURL} />
+      <MailruShare url={shareURL} />
+      <PinterestShare url={shareURL} media="Best Seen on Foot Post" />
+      <PocketShare url={shareURL} />
+      <RedditShare url={shareURL} />
+      <TelegramShare url={shareURL} />
+      <TumblrShare url={shareURL} />
+      <ViberShare url={shareURL} />
+      <VKShareShare url={shareURL} />
+      <WeiboShare url={shareURL} />
+      <WorkplaceShare url={shareURL} />
     </div>
   );
 }
