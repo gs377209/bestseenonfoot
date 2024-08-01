b335e05f1bedb86c006ec65b6c227f4f385ac97e
Gerrod Schirtzinger <gerrod.l.schirtzinger@gmail.com>
2024-08-01 13:59:24 -0400

    add the rest of the stuff

---
+++ src/stories/avatar.stories.ts
@@ -0,0 +1,27 @@
+import Avatar from "@/components/avatar";
+import type { Meta, StoryObj } from "@storybook/react";
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  component: Avatar,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: "centered",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+  title: "Blog/Avatar",
+} satisfies Meta<typeof Avatar>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Primary: Story = {
+  args: {
+    large: false,
+    name: "test",
+    picture: "/assets/authors/lauren.jpg",
+    priority: false,
+  },
+};
