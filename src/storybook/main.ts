baf7611e55dd6e5776f0711dd924e12420fe1ba3
Gerrod Schirtzinger <gerrod.l.schirtzinger@gmail.com>
2025-02-03 15:28:07 -0500

    storybook update

--- src/storybook/main.ts
+++ src/storybook/main.ts
@@ -7,12 +7,21 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
+    "@storybook/addon-mdx-gfm",
   ],
+
+  docs: {},
+
   framework: {
     name: "@storybook/nextjs",
     options: {},
   },
+
   staticDirs: ["../../public"],
   stories: ["../**/*.mdx", "../**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript",
+  },
 };
 export default config;
