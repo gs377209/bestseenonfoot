d9001d7286a17fa5a319e93cbe62dc46500e8aa9
Gerrod Schirtzinger <gerrod.l.schirtzinger@gmail.com>
2025-02-06 15:38:01 -0500

    add some url defaults

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,10 +1,12 @@
 export const BASE_URL =
-  process.env.NEXT_PUBLIC_SITE_URL ?? "https://localhost:3000";
-export const SITE_LOGO = process.env.NEXT_PUBLIC_SITE_LOGO ?? "";
+  process.env.NEXT_PUBLIC_SITE_URL || "https://localhost:3000";
+export const SITE_LOGO = process.env.NEXT_PUBLIC_SITE_LOGO || "";
 export const HOME_OG_IMAGE_URL = BASE_URL + SITE_LOGO;
-export const GOOGLE_ADS_ID = process.env.NEXT_PUBLIC_GOOGLE_ADS_ID;
+export const GOOGLE_ADS_ID = process.env.NEXT_PUBLIC_GOOGLE_ADS_ID || "";
 export const GOOGLE_TAG_MANAGER_ID =
-  process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID;
-export const GOOGLE_OPTIMIZE_ID = process.env.NEXT_PUBLIC_GOOGLE_OPTIMIZE_ID;
-export const FACEBOOK_APP_ID = process.env.NEXT_PUBLIC_FACEBOOK_APP_ID ?? "";
-export const FACEBOOK_PIXEL_ID = process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID;
+  process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID || "";
+export const GOOGLE_OPTIMIZE_ID =
+  process.env.NEXT_PUBLIC_GOOGLE_OPTIMIZE_ID || "";
+export const FACEBOOK_APP_ID = process.env.NEXT_PUBLIC_FACEBOOK_APP_ID || "";
+export const FACEBOOK_PIXEL_ID =
+  process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID || "";
